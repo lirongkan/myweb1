00b40f8a22ff0ffc1da65dca328bc1d74f10fd3e
lirongkan <33339324+lirongkan@users.noreply.github.com>
2019-04-12 23:55:43 +0800

    first commit

---
+++ src/main/HelloServlet.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().write("No1--servlet---time: "+new Date());
+    }
+
+}
